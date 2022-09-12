104255b530833660a8f04e3b0bf3be3f5cafa441
Elena Volkova <111195932+volkhe@users.noreply.github.com>
2022-09-12 23:34:25 +0500

    Delete test_2.spec.ts

--- tests/test_2.spec.ts
+++
@@ -1,6 +0,0 @@
-/*import { test, expect } from '@playwright/test';
-test.only('Тестовый запуск', async({page})=>{
-    await page.goto('https://jpetstore.aspectran.com/catalog/')
-    const locator = page.locator('#MenuContent', {hasText: 'SignUp'})
-    await expect(locator).toBeVisible()
-})*/
